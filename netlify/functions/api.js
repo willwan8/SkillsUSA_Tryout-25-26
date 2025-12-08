e9bede69f1684eea0fa6e64805e74ab13a5b166e
William Wang <williamwang1562@gmail.com>
2025-12-08 15:14:29 -0600

    making things work for netlify

---
+++ netlify/functions/api.js
@@ -0,0 +1,19 @@
+/* server code from last year :D */
+const express = require("express");
+const serverless = require('serverless-http');
+const path = require("path");
+
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+app.get('/{*any}', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+module.exports.handler = serverless(app);
